c076bfa307206e90bffd7d192deb6d51aed7359d
pyanton <pyanton112@gmail.com>
2023-08-11 20:14:49 +0500

    initial commit

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,55 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        UserService userService = context.getBean(UserService.class);
+
+
+        User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+        Car car1 = new Car("BMW", 5, user1);
+        user1.setCar(car1);
+
+        User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+        Car car2 = new Car("AUDI", 3, user2);
+        user2.setCar(car2);
+
+        User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+        Car car3 = new Car("AUDI", 3, user3);
+        user3.setCar(car3);
+
+        User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+        Car car4 = new Car("AUDI", 3, user4);
+        user4.setCar(car4);
+
+        userService.add(user1);
+        userService.add(user2);
+        userService.add(user3);
+        userService.add(user4);
+
+
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println("Car = " + user.getCar());
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println(userService.getUserByCar("AUDI", 3));
+        context.close();
+    }
+}
